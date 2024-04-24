16f0c6ea0fc194496a3ff3fcd3b08cc3ab6fd653
Reaper Gelera <ahoy@barelyhuman.dev>
2024-04-24 14:24:08 +0530

    fix: correct link

--- src/resources/default-md.js
+++ src/resources/default-md.js
@@ -6,7 +6,7 @@ export const defaultMarkdownText = `# sample markdown text
 - built to be to the point and add no overhead for people who simple enjoy writing in markdown 
 
 ## Motivation 
-I write a lot, which you can read on [reaper.im](https://reaper.im) and this tool was primary 
+I write a lot, which you can read on [reaper.is](https://reaper.is) and this tool was primary 
 built to act as a quick markdown app that I could access from anywhere.
 
 `;
